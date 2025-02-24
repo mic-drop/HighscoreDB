f9c92c36f364fb2034ae18491c9fdb02f84af7f3
mic_drop <miguel.saraiva@academiadecodigo.org>
2025-02-24 18:17:06 +0000

    corrects prod persistence value to update

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,5 +1,7 @@
 INSERT INTO game (id, game_name) VALUES (1, 'Snake');
+INSERT INTO game (id, game_name) VALUES (2, 'Super Mario Sunshine');
 
 INSERT INTO player (id, player_name) VALUES (1, 'Mic');
+INSERT INTO player (id, player_name) VALUES (2, 'Drop');
 
 INSERT INTO score (id, date, score, game_id, player_id) VALUES (1, TIMESTAMP '2025-02-02 08:45:56.468', 90000, 1, 1);
