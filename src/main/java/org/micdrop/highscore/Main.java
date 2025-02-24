59e3cfa2eb5ee4c42d946ca8c399ca5be1c1e6d8
mic_drop <miguel.saraiva@academiadecodigo.org>
2025-02-24 18:13:37 +0000

    corrects h2 DB creation path && dev persistence flexible

--- src/main/java/org/micdrop/highscore/Main.java
+++ src/main/java/org/micdrop/highscore/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-        ctx.getEnvironment().setActiveProfiles("prod");
+        ctx.getEnvironment().setActiveProfiles("dev");
 
         ctx.load("/spring/spring-config.xml");
         ctx.refresh();
