a1434eb717e39f08b333ba6e9cde4efd73013cec
mic_drop <miguel.saraiva@academiadecodigo.org>
2025-02-27 14:36:38 +0000

    adds saveOrUpdate to GameDao Integration Test

--- src/test/java/org/micrdop/highscore/persistence/dao/GameJpaDaoIntegrationTest.java
+++ src/test/java/org/micrdop/highscore/persistence/dao/GameJpaDaoIntegrationTest.java
@@ -8,6 +8,8 @@ import org.junit.Assert.*;
 import org.micdrop.highscore.dao.jpa.JpaGameDao;
 import org.micdrop.highscore.model.Game;
 
+import java.util.List;
+
 public class GameJpaDaoIntegrationTest extends JpaIntegrationTestHelper {
 
     private JpaGameDao jpaGameDao;
@@ -32,4 +34,45 @@ public class GameJpaDaoIntegrationTest extends JpaIntegrationTestHelper {
 
     }
 
+    @Test
+    public void findByName(){
+
+        String testName = "Snake";
+
+        Game testGame = jpaGameDao.findGameByName(testName);
+
+        Assert.assertNotNull(testGame);
+        Assert.assertEquals(testName, testGame.getGameName());
+        Assert.assertEquals(1, testGame.getId().intValue());
+
+    }
+
+    @Test
+    public void findAll(){
+        List<Game> testList;
+
+        testList = jpaGameDao.findAll();
+
+        Assert.assertFalse(testList.isEmpty());
+        Assert.assertEquals(1, testList.get(0).getId().intValue());
+        Assert.assertEquals("Snake", testList.get(0).getGameName());
+        Assert.assertEquals(2, testList.get(1).getId().intValue());
+        Assert.assertEquals("Super Mario Sunshine", testList.get(1).getGameName());
+    }
+
+    @Test
+    public void saveOrUpdate(){
+        Game testGame = new Game();
+        testGame.setGameName("Elden Wing");
+
+        tx.beginWrite();
+        testGame = jpaGameDao.saveOrUpdate(testGame);
+        tx.commit();
+
+        Assert.assertNotNull(testGame.getId());
+        Assert.assertEquals(3, testGame.getId().intValue());
+        Assert.assertEquals("Elden Wing", testGame.getGameName());
+
+
+    }
 }
