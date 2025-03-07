2d1f41473845b822b28ad007bb9de849a939b7db
mic_drop <miguel.saraiva@academiadecodigo.org>
2025-03-07 12:29:08 +0000

    adds save test to score dao integration test

--- src/test/java/org/micrdop/highscore/persistence/dao/ScoreJpaDaoIntegrationTest.java
+++ src/test/java/org/micrdop/highscore/persistence/dao/ScoreJpaDaoIntegrationTest.java
@@ -3,23 +3,29 @@ package org.micrdop.highscore.persistence.dao;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.notification.RunListener;
+import org.micdrop.highscore.dao.jpa.JpaGameDao;
+import org.micdrop.highscore.dao.jpa.JpaPlayerDao;
 import org.micdrop.highscore.dao.jpa.JpaScoreDao;
+import org.micdrop.highscore.model.Game;
+import org.micdrop.highscore.model.Player;
 import org.micdrop.highscore.model.Score;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ScoreJpaDaoIntegrationTest extends JpaIntegrationTestHelper{
+public class ScoreJpaDaoIntegrationTest extends JpaIntegrationTestHelper {
     private JpaScoreDao jpaScoreDao;
 
     @Before
-    public void setup(){
+    public void setup() {
         jpaScoreDao = new JpaScoreDao();
         jpaScoreDao.setEm(em);
     }
 
     @Test
-    public void testFindById(){
+    public void testFindById() {
         int id = 1;
 
         Score testScore = jpaScoreDao.findById(id);
@@ -31,7 +37,7 @@ public class ScoreJpaDaoIntegrationTest extends JpaIntegrationTestHelper{
     }
 
     @Test
-    public void testFindAll(){
+    public void testFindAll() {
 
         List<Score> testList = jpaScoreDao.findAll();
 
@@ -40,6 +46,32 @@ public class ScoreJpaDaoIntegrationTest extends JpaIntegrationTestHelper{
         Assert.assertEquals(90000, testList.get(0).getScore().intValue());
         Assert.assertEquals(1, testList.get(0).getId().intValue());
         Assert.assertEquals("Mic", testList.get(0).getPlayer().getPlayerName());
+    }
+
+    @Test
+    public void testSaveUpdate() {
+        JpaGameDao jpaGameDao = new JpaGameDao();
+        JpaPlayerDao jpaPlayerDao = new JpaPlayerDao();
+        jpaGameDao.setEm(em);
+        jpaPlayerDao.setEm(em);
+
+        Score score = new Score();
+        int fakeId = 1;
+        Player player = jpaPlayerDao.findById(fakeId);
+        Game game = jpaGameDao.findById(fakeId);
+
+        score.setScore(777);
+        score.setGame(game);
+        score.setPlayer(player);
+        score.setDate(LocalDateTime.now());
+
+        em.getTransaction().begin();
+        int id = jpaScoreDao.saveOrUpdate(score).getId();
+        em.getTransaction().commit();
+
+        Assert.assertNotNull(jpaScoreDao.findById(id));
+        Assert.assertEquals("id game should be the same", fakeId, score.getGame().getId().intValue());
+        Assert.assertEquals("player id should be the same", fakeId, score.getPlayer().getId().intValue());
     }
 
 
